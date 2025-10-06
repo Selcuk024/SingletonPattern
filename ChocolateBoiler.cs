174e09d057188cb145474b4d4cb11fe5f42221e6
Selcuk <1202320@student.roc-nijmegen.nl>
2025-10-06 14:26:54 +0200

    Add project files.

---
+++ ChocolateBoiler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace Singleton
+{
+    internal class ChocolateBoiler
+    {
+        private bool empty;
+        private bool boiled;
+        private static ChocolateBoiler instance = null;
+        private static readonly object lockObject = new object();
+        private ChocolateBoiler()
+        {
+            empty = true;
+            boiled = false;
+        }
+
+        public static ChocolateBoiler GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (lockObject)
+                {
+                    if (instance == null)
+                    {
+                        instance = new ChocolateBoiler();
+                    }
+                }
+            }
+            return instance;
+        }
+
+        public bool IsEmpty { get { return this.empty; } }
+        public bool IsBoiled { get { return this.boiled; } }
+        public void Fill()
+        {
+            if (empty)
+            {
+                empty = false;
+                boiled = false;
+            }
+        }
+        public void Drain()
+        {
+            if (!empty && boiled)
+            {
+                empty = true;
+            }
+        }
+        public void Boil()
+        {
+            if (!empty && !boiled)
+            {
+                boiled = true;
+            }
+        }
+    }
+}
